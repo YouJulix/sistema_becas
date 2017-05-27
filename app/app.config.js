a7a56fc76ae50ef08143820172137a7d7491eece
JulioAndresPadillaCruz <julioandres.padilla@gmail.com>
2017-05-27 17:46:17 -0500

    Creada estructura html y plantilla de login.

---
+++ app/app.config.js
@@ -0,0 +1,12 @@
+angular.
+	module('sistBecasApp').
+		config(['$locationProvider', '$routeProvider',
+			function config($locationProvider, $routeProvider){
+				$locationProvider.hashPrefix('!');// We also used $locationProvider.hashPrefix() to set the hash-prefix to !. This prefix will appear in the links to our client-side routes, right after the hash (#) symbol and before the actual path (e.g. index.html#!/some/path).
+				//Setting a prefix is not necessary, but it is considered a good practice (for reasons that are outside the scope of this tutorial). ! is the most commonly used prefix.
+				//Al agregar este prefijo, en nuestros enlaces debemos colocar dicho prefijo: ejemplo:  <a href="#!/phones/{{phone.id}}>..."
+				$routeProvider.when('/login',{
+					template: '<login-becas></login-becas>'
+				}).otherwise('/login');
+			}
+		]);
---
+++ app/login-becas/login-becas.component.js
@@ -0,0 +1,10 @@
+angular.
+	module('loginBecas').
+		component('loginBecas', {
+			templateUrl: 'login-becas/login-becas.template.html',
+			controller: function LoginBecasController(){
+				var self = this;
+				self.variable = 5;
+			}
+		});
+
