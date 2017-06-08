39700f4d435122589b3d3093e0b2987a6af9ea35
CelinaLusan <rodelusa10@gmail.com>
2017-06-08 00:39:09 -0500

    se agrego el modulo ingresos mensuales

--- public/app.module.js
+++ public/app.module.js
@@ -7,14 +7,7 @@ angular.module('sistBecasApp',[
 	'editAdminInfo'
 	'dependenciaEconomica',
 	'gastosAlumno',
-<<<<<<< HEAD
 	'informacionHogar',
-	'moduloBeca2'
-=======
+	'moduloBeca2',
 	'informacionHogar'
-<<<<<<< HEAD
-=======
->>>>>>> b54bdaf27d7f8e9aa81564cacc7e4a426b8f0850
->>>>>>> f4674576b6541888c5144fbbef52961ff5215aca
->>>>>>> bf560b1228ecdcf2894e13ce5213494d1d45d81f
 	]);
