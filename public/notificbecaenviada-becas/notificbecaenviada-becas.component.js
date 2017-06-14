26a70526dbde17a851d20de3e341511571f66c19
javcruz62 <javcruz62@gmail.com>
2017-06-13 23:42:02 -0500

    notificacion beca enviada

--- public/notificbecaenviada-becas/notificbecaenviada-becas.component.js
+++ public/notificbecaenviada-becas/notificbecaenviada-becas.component.js
@@ -1,1 +1,43 @@
+angular.
+	module('notificbecaenviadaBecas').
+		component('notificbecaenviadaBecas', {
+			templateUrl: 'notificbecaenviada-becas/notificbecaenviada-becas.template.html',
 
+			controller:['$http', '$scope',
+				function NotificbecaenviadaBecasController($http,$scope){
+					var self = this; //BUena practica es no manipular el this directamente
+					//self.dataValid = true; // Boleano que servirá para saber si se muestra o no un mensaje de error.//Al inicio no se muestra
+					//self.validateData = function(){
+
+						//alert("hola");
+					//}
+					$scope.matricula = "";
+					//localStorage.setItem("matricula","23232");
+					
+
+					
+
+					self.validar = function(){
+						$scope.matricula = localStorage.getItem("matricula");
+
+
+						console.log(localStorage.getItem("matricula"));
+
+						if($scope.matricula == null){
+							window.location = "/#!/login";
+						}
+					};
+
+					self.validar();
+
+
+			   		
+				    $scope.cerrarSesion = function() {
+				    	//alert("entra");
+				        localStorage.removeItem("matricula");
+				        window.location = "/#!/login";
+				    }
+
+				}
+				]
+		});
