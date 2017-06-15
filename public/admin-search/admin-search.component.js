d9d66ae0cc63875d16543542b33b05fa0fa5a0ba
alerd0895 <alerd453@gmail.com>
2017-06-15 17:39:29 -0500

    cambio localhost por ip

--- public/admin-search/admin-search.component.js
+++ public/admin-search/admin-search.component.js
@@ -11,13 +11,13 @@ angular.
 						//console.log(self.matricula);
 						object = $http({
 						method 	: 	'GET',
-						url 	: 	'http://localhost:8000/api/v1.0/users/'+ self.matricula
+						url 	: 	'http://192.168.43.247:8000/api/v1.0/users/'+ self.matricula
 						}).success(function(data){
 							$scope.dato = data[0];
 							console.log($scope.dato);
 							object = $http({
 								method : 'GET',
-								url    : 'http://localhost:8000/api/v1.0/solicitudes/'+ self.matricula 
+								url    : 'http://192.168.43.247:8000/api/v1.0/solicitudes/'+ self.matricula 
 							}).success(function(data){
 								$scope.solicitudes = [];
 								$scope.solicitud = data;
@@ -47,7 +47,7 @@ angular.
 						if(confirmar){  
 					        $http({ 
 					            method: 'DELETE',
-								url: 'http://localhost:8000/api/v1.0/solicitudes/id/' + idmat
+								url: 'http://192.168.43.247:8000/api/v1.0/solicitudes/id/' + idmat
 					        }).success(function(data){
 					        	self.busqueda();
 					        	window.location = "/#!/admin_seach";
