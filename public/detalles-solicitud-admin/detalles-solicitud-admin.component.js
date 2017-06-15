7812c104126c9dbac14ee4f2a258c41b5f875f4a
VitorManueloky <arguellesalcazar101@gmail.com>
2017-06-15 06:09:27 -0500

    detalles-solicitud-admin

--- public/detalles-solicitud-admin/detalles-solicitud-admin.component.js
+++ public/detalles-solicitud-admin/detalles-solicitud-admin.component.js
@@ -27,90 +27,6 @@ angular.
 					//================================================================
 					*/
 
-					self.carreras = ['Lic. en Informática',
-									'Ing. en Agronomía',
-									'Lic. en Administración'];
-
-					self.semestres = ['1°','2°','3°','4°','5°','6°','7°','8°','9°','10°'];
-
-					self.grupos = [];
-					self.valueGrupo = function(){
-						if(self.carrera == 'Lic. en Informática'){
-							if (self.semestre == '1°'){
-								self.grupos = ['101-A'];
-							}else if (self.semestre == '2°') {
-								self.grupos = ['201-A'];
-							}else if (self.semestre == '3°'){
-								self.grupos = ['301-A'];
-							}else if (self.semestre == '4°'){
-								self.grupos = ['401-A'];
-							}else if (self.semestre == '5°'){
-								self.grupos = ['501-A'];
-							}else if (self.semestre == '6°'){
-								self.grupos = ['601-A'];
-							}else if (self.semestre == '7°'){
-								self.grupos = ['701-A'];
-							}else if (self.semestre == '8°'){
-								self.grupos = ['801-A'];
-							}else if (self.semestre == '9°'){
-								self.grupos = ['901-A'];
-							}else if (self.semestre == '10°'){
-								self.grupos = ['1001-A'];
-							}
-						}else if (self.carrera == 'Ing. en Agronomía'){
-							if (self.semestre == '1°'){
-								self.grupos = ['102-A'];
-							}else if (self.semestre == '2°') {
-								self.grupos = ['202-A'];
-							}else if (self.semestre == '3°'){
-								self.grupos = ['302-A'];
-							}else if (self.semestre == '4°'){
-								self.grupos = ['402-A'];
-							}else if (self.semestre == '5°'){
-								self.grupos = ['502-A'];
-							}else if (self.semestre == '6°'){
-								self.grupos = ['602-A'];
-							}else if (self.semestre == '7°'){
-								self.grupos = ['702-A'];
-							}else if (self.semestre == '8°'){
-								self.grupos = ['802-A'];
-							}else if (self.semestre == '9°'){
-								self.grupos = ['902-A'];
-							}else if (self.semestre == '10°'){
-								self.grupos = ['1002-A'];
-							}
-						}else if (self.carrera == 'Lic. en Administración') {
-							if (self.semestre == '1°'){
-								self.grupos = ['103-A','103-B'];
-							}else if (self.semestre == '2°') {
-								self.grupos = ['203-A','203-B'];
-							}else if (self.semestre == '3°'){
-								self.grupos = ['303-A','303-B'];
-							}else if (self.semestre == '4°'){
-								self.grupos = ['403-A','403-B'];
-							}else if (self.semestre == '5°'){
-								self.grupos = ['503-A','503-B'];
-							}else if (self.semestre == '6°'){
-								self.grupos = ['603-A','603-B'];
-							}else if (self.semestre == '7°'){
-								self.grupos = ['703-A','703-B'];
-							}else if (self.semestre == '8°'){
-								self.grupos = ['803-A','803-B'];
-							}else if (self.semestre == '9°'){
-								self.grupos = ['903-A','903-B'];
-							}else if (self.semestre == '10°'){
-								self.grupos = ['1003-A','1003-B'];
-							}
-						}
-					}
-
-					self.estadosCivil = ['Soltero/a','Casado/a','Viudo/a','Divorciado/a'];
-
-					self.estados = ['Oaxaca','Puebla','Guerrero','Chiapas','Veracruz'];
-					self.estado = self.estados[0];
-
-					self.parentescos = ['Padre','Madre','Abuelos','Tio/a','Primo/a'];
-					
 					self.matricula = localStorage.getItem('matricula');
 					self.solicitudId = localStorage.getItem('idSolicitud');
 					
@@ -144,7 +60,6 @@ angular.
 							self.nombreHuesped = data[0].nombreHuesped;
 							self.parentesco = data[0].parentesco;
 
-							self.valueGrupo();
 						}).
 						error(function(){
 							alert("Error. El Usuario no existe");
@@ -177,7 +92,60 @@ angular.
 						error(function(){
 							alert("Error.");
 						});
+
+					};
+					self.saveData = function(){
+						self.data =  {
+							"estado" 				: self.estado,
+							"porcentaje_sugerido"	:self.porcentaje_sugerido,
+							"porcentaje_final"		:self.porcentaje_final,
+							"libre_de_extra"		:self.libre_de_extra,	
+							"biblioteca_completa"	:self.biblioteca_completa,
+							"fecha_envio"			:self.fecha_envio						
+						};
+						$http({
+							method:'PUT',
+							url: 'http://localhost:8000/api/v1.0/solicitudes/'+ self.matricula
+							data : self.data,  
+						}).
+						success(function(data){
+							alert("Datos guardados");
+							window.location = "#!/login";
+						}).
+						error(function(){
+							alert("Error.");
+						});
+					};
+
+					self.bibliotecaCambio = function(){
+						if(self.biblioteca_completa){
+							self.porcentaje_final = self.porcentaje_sugerido; 
+						}
+						else{
+							self.porcentaje_final = self.porcentaje_sugerido - 25; 
+						}
+					};
+
+					self.extraCambio = function(){
+						if(self.libre_de_extra){
+							if(self.biblioteca_completa){
+								self.porcentaje_final = self.porcentaje_sugerido - 25; 
+							}
+							else{
+								self.porcentaje_final = self.porcentaje_final; 
+							}	
+						}
+						else{
+							if(self.biblioteca_completa){
+								self.porcentaje_final = self.porcentaje_sugerido; 
+							}
+							else{
+								self.porcentaje_final = self.porcentaje_final; 
+							}	
+						}
+
 					}
+
 					this.renderUser(self.matricula);
 				}
 			]
